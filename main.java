df61d65b93b06f70e9313b6cdd7f4b03e29bd80f
Rykkartt <georges.leuch@gmail.com>
2023-05-09 19:04:00 +0200

    commitment

--- main.java
+++ main.java
@@ -1,9 +1,9 @@
 import tasks.Task;
-import tasks.Task1;
+import tasks.Task6;
 
 class GitAssessment {
     public static void main(String[] args) {
-        Task[] tasks = { new Task1() };
+        Task[] tasks = { new Task6() };
         for (Task task: tasks) {
             if (task.test()) {
                 System.out.println(task.getTaskName() + ": " + "passed");
